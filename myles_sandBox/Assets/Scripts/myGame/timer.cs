9fe5532959ce2185a39d200b16c232109d68fbf4
MylesSteel <90920295+MylesSteel@users.noreply.github.com>
2021-10-09 13:01:00 -0600

    TIMER!
    followed a tutorial to get timer on screen. tweeked it to work with int instead of float and display as seconds instead of 00:00

--- myles_sandBox/Assets/Scripts/myGame/timer.cs
+++ myles_sandBox/Assets/Scripts/myGame/timer.cs
@@ -5,33 +5,30 @@ using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
-    public float timeValue = 60;
-    public Text timerText;
+    public GameObject textDisplay;
+    public int timeValue = 60;
+    public bool takingAway1 = false;
     // Update is called once per frame
+
+    private void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "" + timeValue;
+    }
     void Update()
     {
-        if(timeValue > 0)
-            {
-            timeValue -= Time.deltaTime; 
-        }
-        else
+        if (takingAway1 == false && timeValue > 0)
         {
-            timeValue = 0; 
+            StartCoroutine(timerDown());
         }
-        
-    }
-
-    void DisplayTime(float time)
-    {
-        if(time < 0 )
+        IEnumerator timerDown()
         {
-            time = 0; 
+            takingAway1 = true;
+            yield return new WaitForSeconds(1);
+            timeValue -= 1;
+            textDisplay.GetComponent<Text>().text = "" + timeValue;
+            takingAway1 = false;
         }
-        float seconds = Mathf.FloorToInt(time % 60);
 
-        timerText.text = string.Format("{0:00}", seconds);
     }
 
 }
-
- 
