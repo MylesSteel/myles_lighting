79729e4f67a20d4e0e2c73603f3c091704b2a4fd
MylesSteel <myles.aitken-steel@lethbridgecollege.ca>
2021-10-12 15:20:50 -0600

    Moved repo
    My OnMouseDown was conflicting with the XR head set input. it has disabled input in my sandbox so I moved the whole game to a separate repo. I got OnMouseDown functionality and I can loop my guess but it shows incorrect.

--- myles_sandBox/Assets/Scripts/newGuessingGame/click.cs
+++ myles_sandBox/Assets/Scripts/newGuessingGame/click.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Click : MonoBehaviour
 {
     
-    //public Guess gs;           //refrence to guess script to grab public varriables guesses array and guesscount int. 
+    public Guess gs;           //refrence to guess script to grab public varriables guesses array and guesscount int. 
 
-    private void Start()
-    {
-       // gs = GameObject.Find("guessEmpty").GetComponent<Guess>();
-    }
+   // void Start()
+    //{
+        //gs = GameObject.Find("guessEmpty").GetComponent<Guess>();
+   // }
 
 
 
@@ -19,9 +19,9 @@ public class Click : MonoBehaviour
     public void OnMouseDown()
     {
         Debug.Log("clicked");
-        //gs.guesses[gs.guessCount] = gameObject; //each click on game object is stored in guesses array 
-       // gs.guessCount++; //increase guesscount by 1 on each click.
-       // gs.PlayerGuessCheck();  //compares guesses to recipe script gameobject array correctGuess.
+        gs.guesses[gs.guessCount] = gameObject; //each click on game object is stored in guesses array 
+        gs.guessCount++; //increase guesscount by 1 on each click.
+        gs.PlayerGuessCheck();  //compares guesses to recipe script gameobject array correctGuess.
 
     }  //registers input on click
 
