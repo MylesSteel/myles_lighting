8988e33ae87cadb3f1412f9bf3ee17853df0284c
MylesSteel <90920295+MylesSteel@users.noreply.github.com>
2021-10-19 00:47:33 -0600

    gun
    Bullets almost shoot when spawned. I don't have errors so im not sure why they wont apply force.

--- myles_sandBox/Assets/Scripts/InteractionProj/Bullet.cs
+++ myles_sandBox/Assets/Scripts/InteractionProj/Bullet.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class Bullet : MonoBehaviour
 {
+    Rigidbody rB;
     private XRGrabInteractable grabInteractable; //refrence
     [SerializeField]
     GameObject bulletPrefab;                     //game object for bullet
     [SerializeField]
     Transform spawnPoint;                        //spawn location for bullet at end of gun
+    public ParticleSystem muzzleFlash;
     void Awake()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
@@ -19,5 +21,7 @@ public class Bullet : MonoBehaviour
     private void ShootBullet(ActivateEventArgs arg0)
     {
         Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
+        rB.AddForce(bulletPrefab.transform.forward * 1000); 
+        //muzzleFlash.Play();    
     }
 }
