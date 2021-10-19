2e7131a650627ba97f28509b2bd4da6955093a69
MylesSteel <90920295+MylesSteel@users.noreply.github.com>
2021-10-19 17:57:30 -0600

    bullets
    bullets now fire down the lane.

--- myles_sandBox/Assets/Scripts/InteractionProj/BulletFire.cs
+++ myles_sandBox/Assets/Scripts/InteractionProj/BulletFire.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class BulletFire : MonoBehaviour
 {
     Rigidbody rB;
+    [SerializeField]
     float speed = 0.01f;
-    //float lifeSpan = 1.5f;
+    [SerializeField]
+    float lifeSpan = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         rB = GetComponent<Rigidbody>();
-        rB.AddForce(Vector3.forward * (speed / 2), ForceMode.VelocityChange);
+        rB.AddForce(Vector3.forward * (speed), ForceMode.Impulse);
      
     }
 
-    // Update is called once per frame
-   // void Update()
-   // {
-    //    if (lifeSpan >= 0)
-   //     {
-    //        lifeSpan -= Time.deltaTime;
-   //     }
-    //    else
-   //     {
-   //         Destroy(gameObject);
-     //   }
-  //  }
+     //Update is called once per frame
+    void Update()
+    {
+        if (lifeSpan >= 0)
+        {
+            lifeSpan -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
